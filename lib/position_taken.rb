6c92c8a3c2f5a33238396b688d01a0297fe5643a
gorillamunch <erycarockwell@gmail.com>
2016-04-08 22:24:13 +0000

    Done.

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,1 +1,17 @@
-# code your #position_taken? method here!
+# code your #position_taken? met
+def position_taken?(board, position)
+  # basic solution:
+
+  # taken = nil
+  # if (board[position] ==  " " || board[position] == "" || board[position] == nil)
+  #   taken = false
+  # else
+  #   taken = true
+  # end
+  # taken
+
+  # advanced solution w/ ternary operator
+  (board[position] ==  " " || board[position] == "" || board[position] == nil) ?
+    false : true
+
+end
